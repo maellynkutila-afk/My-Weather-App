f6beac82d2f21e1f9849996a19df9205e3743fb4
maellynkutila-afk <maellynkutila@gmail.com>
2026-09-18 12:24:37 +0200

    Added basic search engine (w/o API)

--- src/index.js
+++ src/index.js
@@ -29,10 +29,19 @@ let month = monthsOfYear[current.getMonth()];
 let date = current.getDate();
 let year = current.getFullYear();
 
-console.log(year);
-
 let h2 = document.querySelector("h2");
 h2.innerHTML = `${day} ${month} ${date}, ${year}`;
+
+function changeDisplay(event) {
+  event.preventDefault();
+  let city = document.querySelector("#entered-city");
+  let cityValue = city.value;
+  let h3 = document.querySelector("h3");
+  h3.innerHTML = `${cityValue}`;
+}
+
+let weatherForm = document.querySelector("#weather-form");
+weatherForm.addEventListener("submit", changeDisplay);
 
 let apiKey = "4b4301acf33210b672de34o3f362t059";
 let apiUrl =
